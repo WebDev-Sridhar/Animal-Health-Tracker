5fe7bb5dbe36f6c4fa23c713bb5811d36ffc9aca
WebDev-Sridhar <sridhartamilselvam22@gmail.com>
2026-03-08 15:36:27 +0530

    Log full API response data in fetchAnimals function on AdoptionPage

--- frontend/src/pages/AdoptionPage.jsx
+++ frontend/src/pages/AdoptionPage.jsx
@@ -23,7 +23,7 @@ export default function AdoptionPage() {
   const fetchAnimals = async () => {
     try {
       const res = await apiClient.get("/reports/adoptions");
-      console.log(res.data.data)
+      console.log(res.data)
    setAnimals(res.data.data);
     //   setFiltered(adoptionAnimals);
     } catch (err) {
