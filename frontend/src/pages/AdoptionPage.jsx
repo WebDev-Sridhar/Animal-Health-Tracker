624fa001631c34a36a2dfca049ff203a0e63cdc4
WebDev-Sridhar <sridhartamilselvam22@gmail.com>
2026-03-08 23:30:07 +0530

    location icon changed

--- frontend/src/pages/AdoptionPage.jsx
+++ frontend/src/pages/AdoptionPage.jsx
@@ -188,7 +188,20 @@ export default function AdoptionPage() {
               rel="noopener noreferrer"
               className="flex items-center gap-2 text-blue-600 text-sm mt-2 hover:underline"
             >
-              📍 Get Directions
+                     
+                    <svg
+                      className="w-4 h-4 mr-2"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                   Get Directions
+                  
             </a>
           )}
 
